c48e61c92826e208950650db2bf3d8c07418920b
Kurtis Heimerl <kheimerl@cs.berkeley.edu>
2012-12-21 23:36:38 -0800

    now cross-network

--- vbtspa/scripts/VBTS_PA_On.py
+++ vbtspa/scripts/VBTS_PA_On.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 
 import xmlrpclib
+import argparse
 
-PORT = 8080
-SERVER = "localhost"
+parser = argparse.ArgumentParser(description='Turn off a VBTS PA')
+parser.add_argument("-p", "--port", dest="port", 
+                    default="8080", help="The port of the server")
+parser.add_argument("-d", "--dest", dest="dest", 
+                    default="localhost", help="The IP or hostname of the server")
 
-s = xmlrpclib.ServerProxy('http://' + SERVER + ":" + str(PORT))
+args = parser.parse_args()
+
+s = xmlrpclib.ServerProxy('http://' + args.dest + ":" + args.port)
 
 s.on()
